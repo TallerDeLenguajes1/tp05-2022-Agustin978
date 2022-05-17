75503eb17f3b18906335a97f27536119aa28660d
Agustin978 <loboagus10@gmail.com>
2022-05-17 15:50:42 -0300

    primer push

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string texto = Console.ReadLine();
+int numero = 122;
+Console.WriteLine("Usted escribio "+texto+" y el numero es: "+numero);
