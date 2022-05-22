6e1c7336f94f7f3f391cfc0a0c93d2e14126508c
Agustin978 <loboagus10@gmail.com>
2022-05-22 11:41:49 -0300

    Ingresada la inversion de numeros en la rama CalculadoraV1

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,5 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//using System.Math;
+
 Console.WriteLine("Hello, World!");
-string texto = Console.ReadLine();
-int numero = 122;
-Console.WriteLine("Usted escribio "+texto+" y el numero es: "+numero);
+Console.WriteLine("Ingrese el numero que desea invertir");
+//int num = Console.ReadLine();
+char[] numero = Console.ReadLine().ToCharArray();
+Array.Reverse(numero);
+string num = new string(numero);
+
+if (!string.IsNullOrEmpty(num))
+{
+    Console.WriteLine("El numero invertido es: " + num);
+}else
+{
+    Console.WriteLine("No se ingreso ningun numero a invertir :v");
+}
+
+
+
+//int numero = 122;
+//Console.WriteLine("Usted escribio "+texto+" y el numero es: "+numero);
+
